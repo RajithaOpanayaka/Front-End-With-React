b4e796fa377fb6995ef44563786192d6e0eee8d0
RajithaOpanayaka <rajithaopanayaka.ro@gmail.com>
2020-11-29 15:25:30 +0530

    add aboutcomponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import {COMMENTS} from '../shared/comments';
 import {LEADERS} from '../shared/leaders';
 import {PROMOTIONS} from '../shared/promotions';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
@@ -36,7 +37,12 @@ class Main extends Component{
         <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
         comments={this.state.comments.filter((comment)=>comment.dishId===parseInt(match.params.dishId,10))}       
         />
-      );
+      ); 
+  }
+  const AboutPage=()=>{
+    return(
+      <About leaders={this.state.leaders}/>
+    );
   }
   return (
     
@@ -46,7 +52,8 @@ class Main extends Component{
               <Route path="/home" component={HomePage}/>
               <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
               <Route path="/menu/:dishId" component={DishWithId}/>
-              <Route exact path="/contactus" component={Contact}/>  
+              <Route exact path="/contactus" component={Contact}/>
+              <Route path="/aboutus" component={AboutPage}/>  
               <Redirect to="/home"/>
           </Switch>
       
